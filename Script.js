d59b26ba23b717b924f5a333c1acd30bb5cd278e
shawn bailey <shwnmbailey@gmail.com>
2023-01-21 16:17:32 -0500

    "Script.js"

--- Script.js
+++ Script.js
@@ -1,82 +1,118 @@
 let pattern = [];
 let playerPattern = [];
-let flash
-let playerTurn;
-let cmpterTurn;
+let flash;
+let round;
 let good;
-let interval;
-let strictMode = false;
-let powerOn = false;
+let pcTurn;
+let intervalid;
+let strict = false;
+let on = false;
 let win;
 
-const roundIndicator = document.querySelector("#round");
+const roundCounter = document.querySelector("#ROUND");
 const green = document.querySelector("#green");
 const red = document.querySelector("#red");
 const yellow = document.querySelector("#yellow");
 const blue = document.querySelector("#blue");
-const strctMode = document.querySelector("#strict-mode");
-const pwrOn = document.querySelector("#on");
-const startBtn = document.querySelector("#start");
+const strictBtn = document.querySelector("#strict-mode");
+const onBtn = document.querySelector("#on");
+const startbtn = document.querySelector("#start-button");
 
-strctMode.addEventListener('click', (event) => {
-    if (strctMode.checked == true) {
-        strictMode = true;
+strictBtn.addEventListener('click', (event) => {
+    if (strictBtn.checked == true) {
+        strict = true;
     } else {
-        strictMode = false;
+        strict = false;
     }
 });
 
-pwrOn.addEventListener('click', (event) => {
-    if (pwrOn.checked == true) {
-        powerOn = true;
+onBtn.addEventListener('click', (event) => {
+    if (onBtn.checked == true) {
+        on = true;
+        roundCounter.innerHTML = "-";
     } else {
-        powerOn = false;
+        on = false;
+        roundCounter.innerHTML = "";
+        clearColor();
+        clearInterval(intervalid);
     }
 });
 
-startBtn.addEventListener('click', (event) => {
+startbtn.addEventListener('click', (event) => {
     if (on || win) {
-        playGame();
+        play();
     }
-})
+});
 
-function playGame() {
+function play() {
     win = false;
-    pattern = [];
+    pattern = [1, 1, 2, 4];
     playerPattern = [];
     flash = 0;
-    interval = 0;
-    playerTurn = 1;
-    roundIndicator.innerHTML = 1;
+    intervalid = 0;
+    round = 1;
+    roundCounter.innerHTML = 1;
     good = true;
-    for (let i = 0; i < 20; i++) {
+    for (var i = 0; i < 20; i++) {
         pattern.push(Math.floor(Math.random() * 4) + 1);
     }
-    cmpterTurn = true;
+    pcTurn = true;
 
-    interval = setInterval(gameTrn, 800);
+    intervalid = setInterval(gameTurn, 800);
 }
 
-function gameTrn() {
-    powerOn = false;
+function gameTurn() {
+    on = false;
 
-    if (flash == playerTurn) {
-        clearInterval(interval);
-        cmpterTurn = false;
+    if (flash == round) {
+        clearInterval(intervalid);
+        pcTurn = false;
         clearColor();
-        powerOn = true;
+        on = true;
     }
 
-    if (cmpterTurn) {
+    if (pcTurn) {
         clearColor();
         setTimeout(() => {
             if (pattern[flash] == 1) one();
             if (pattern[flash] == 2) two();
-            if (pattern[flash] == 3) three();
+            if (pattern[flash] == 3) three()
             if (pattern[flash] == 4) four();
             flash++;
         }, 200);
     }
+}
+
+function one() {
+    if (pcTurn) {
+
+    }
+
+    green.style.backgroundColor = "lightgreen";
+}
+
+function two() {
+    if (pcTurn) {
+
+    }
+
+    red.style.bacgroundColor = "tomato";
+}
+
+function three() {
+    if (pcTurn) {
+
+    }
+    noise = true;
+    yellow.style.backgroundColor = "yellow";
+}
+
+function four() {
+    if (pcTurn) {
+
+    }
+
+    blue.style.backgroundColor = "lightskyblue";
 }
 
 function clearColor() {
@@ -94,61 +130,95 @@ function flashColor() {
 }
 
 green.addEventListener('click', (event) => {
-    if (powerOn) {
+    if (on) {
         playerPattern.push(1);
         check();
-        one();
         if (!win) {
-            setTimeout(
-                () => {
-                    clearColor
-                }, 300)
+            setTimeout(() => {
+                clearColor();
+            }, 300);
         }
     }
 })
 
 red.addEventListener('click', (event) => {
-    if (powerOn) {
+    if (on) {
         playerPattern.push(2);
         check();
-        two();
         if (!win) {
-            setTimeout(
-                () => {
-                    clearColor
-                }, 300)
+            setTimeout(() => {
+                clearColor();
+            }, 300);
         }
     }
 })
 
 yellow.addEventListener('click', (event) => {
-    if (powerOn) {
+    if (on) {
         playerPattern.push(3);
         check();
-        three();
         if (!win) {
-            setTimeout(
-                () => {
-                    clearColor
-                }, 300)
+            setTimeout(() => {
+                clearColor();
+            }, 300);
         }
     }
 })
 
 blue.addEventListener('click', (event) => {
-    if (powerOn) {
+    if (on) {
         playerPattern.push(4);
         check();
-        four();
         if (!win) {
-            setTimeout(
-                () => {
-                    clearColor
-                }, 300)
+            setTimeout(() => {
+                clearColor();
+            }, 300);
         }
     }
 })
 
 function check() {
-    if( playerPattern[playerPattern.length -1] !== pattern[playerPattern.length])
+    if (playerPattern[playerPattern.length - 1] !== pattern[playerPattern.length - 1])
+        good = false;
+
+    if (playerPattern.length == 3 && good) {
+        winGame();
+    }
+
+    if (good == false) {
+        flashColor();
+        roundCounter.innerHTML = "!";
+        setTimeout(() => {
+            roundCounter.innerHTML = round;
+            clearColor();
+
+            if (strict) {
+                play();
+            } else {
+                pcTurn = true;
+                flash = 0;
+                playerPattern = [];
+                good = true;
+                intervalid = setInterval(gameTurn, 800);
+            }
+        }, 800);
+
+    }
+
+    if (round == playerPattern.length && good && !win) {
+        round++;
+        playerPattern = [];
+        pcTurn = true;
+        flash = 0;
+        roundCounter.innerHTML = round;
+        intervalid = setInterval(gameTurn, 800);
+    }
+
+}
+
+function winGame() {
+    flashColor();
+    roundCounter.innerHTML = "WIN!";
+    on = false;
+    win = true;
 }
